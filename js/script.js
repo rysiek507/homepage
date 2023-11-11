44180068f766d89bbbd5f5bcb33c9e9a4b5d932e
Radoslaw Siek <radosaw.siek@gmail.com>
2023-11-11 19:55:18 +0100

    change function to array function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-const spoiler = document.querySelector(".js__spoiler");
-const spoilerTextHide = "Spoiler";
-const spoilerButton = document.querySelector(".js__spoilerButton");
-const spoilerTextShow =
-  "Istnieją teorie, że Marvin jest Blizną pojawiającym się w Gothic I";
-let changeSpoilerText = () => {
-  spoiler.innerText =
-    spoiler.innerText === spoilerTextHide ? spoilerTextShow : spoilerTextHide;
-};
-let changeSpoilerButtonText = () => {
-  spoilerButton.innerText =
-    spoiler.innerText === spoilerTextHide ? "Pokaż spoiler" : "Ukryj Spoiler";
-};
-spoilerButton.addEventListener("click", changeSpoilerText);
-spoilerButton.addEventListener("click", changeSpoilerButtonText);
+{
+  const changeSpoilerText = () => {
+    spoiler.innerText =
+      spoiler.innerText === spoilerTextHide ? spoilerTextShow : spoilerTextHide;
+  };
+  const changeSpoilerButtonText = () => {
+    spoilerButton.innerText =
+      spoiler.innerText === spoilerTextHide ? "Pokaż spoiler" : "Ukryj Spoiler";
+  };
+  const init = () => {
+    spoilerButton.addEventListener("click", changeSpoilerText);
+    spoilerButton.addEventListener("click", changeSpoilerButtonText);
+  };
+  const spoiler = document.querySelector(".js__spoiler");
+  const spoilerTextHide = "Spoiler";
+  const spoilerButton = document.querySelector(".js__spoilerButton");
+  const spoilerTextShow =
+    "Istnieją teorie, że Marvin jest Blizną pojawiającym się w Gothic I";
+  init();
+}
